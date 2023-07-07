823a24a7f73ee04d8a2689407d18baeedd0a1445
Firmanrachmad <dragobniel@gmail.com>
2023-07-08 06:17:54 +0700

    update

--- server.js
+++ server.js
@@ -3,10 +3,40 @@ const mysql = require("mysql");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+app.set("view engine", "ejs");
+app.set("views", "views");
+const db = mysql.createConnection({
+  host: "localhost",
+  database: "to-do-list",
+  user: "root",
+  password: "",
+});
+
+db.connect((err) => {
+  // if (err) throw err
+  console.log("DB Connected");
+  const sql = "SELECT * FROM todolist";
+
+  db.query(sql, (err, result) => {
+    const users = JSON.parse(JSON.stringify(result));
+    console.log("Result = ", users);
+
+    //get data
+    app.get("/", (req, res) => {
+      res.render("index", { users: users, task_name:"To-Do-List App" });
+    });
+
+    //insert data
+    app.post("/insert", (req, res) => {
+        const insert = `INSERT INTO todolist (task_name, description, is_completed) VALUES ()`
+        db.query(insert, (err, result) => {
+            
+        })
+
+    });
+  });
+});
 
 app.listen(8000, () => {
-    console.log("Listening to port 8000");
-})
+  console.log("Listening to port 8000");
+});
